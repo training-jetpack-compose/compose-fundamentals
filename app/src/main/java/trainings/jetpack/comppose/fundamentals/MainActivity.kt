ba320128c4665493e03083d352ad8139445d4881
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-20 22:39:39 +0200

    premiere personnalisation d'un label dans un @Preview.

--- app/src/main/java/trainings/jetpack/comppose/fundamentals/MainActivity.kt
+++ app/src/main/java/trainings/jetpack/comppose/fundamentals/MainActivity.kt
@@ -3,14 +3,28 @@ package trainings.jetpack.comppose.fundamentals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontStyle.Companion.Italic
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import trainings.jetpack.comppose.fundamentals.R.color.purple_700
 import trainings.jetpack.comppose.fundamentals.ui.theme.TrainingJetPackCompposeFundamentalsTheme
+
+
+const val jetpack_compose = "jetpack compose"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting(jetpack_compose)
                 }
             }
         }
@@ -31,13 +45,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hi, $name!",
+        fontStyle = Italic,
+        color = colorResource(id = purple_700),
+        fontSize = 18.sp,
+        fontWeight = Bold
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TrainingJetPackCompposeFundamentalsTheme {
-        Greeting("Android")
+        Greeting(jetpack_compose)
     }
 }
