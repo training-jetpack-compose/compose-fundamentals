8c0bf3a47cd6ada7036f7a0ba23d2890cfb91c9f
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-01 22:24:17 +0200

    clean code

--- app/src/androidTest/java/jetpackcompose/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/jetpackcompose/ExampleInstrumentedTest.kt
@@ -2,25 +2,19 @@ package jetpackcompose
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
-  @Test
-  fun useAppContext() {
-    // Context of the app under test.
-    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-      Assert.assertEquals("jetpackcompose.fundamentals", appContext.packageName)
-    println(appContext.packageName)
-    println(appContext.applicationInfo)
-
-  }
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("jetpackcompose.fundamentals", appContext.packageName)
+        println(appContext.packageName)
+        println(appContext.applicationInfo)
+    }
 }
