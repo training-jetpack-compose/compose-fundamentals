c17bc51e3315a36a6f716546ae24969741df1ac9
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-02 14:51:59 +0200

    TextScreen

--- app/src/main/java/jetpackcompose/screens/TextScreen.kt
+++ app/src/main/java/jetpackcompose/screens/TextScreen.kt
@@ -3,9 +3,17 @@ package jetpackcompose.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import jetpackcompose.R
 import jetpackcompose.router.BackButtonHandler
 import jetpackcompose.router.JetFundamentalsRouter.navigateTo
 import jetpackcompose.router.Screen
@@ -17,11 +25,16 @@ fun TextScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) { MyText() }
-
     BackButtonHandler { navigateTo(Screen.Navigation) }
 }
 
+@Preview
 @Composable
 fun MyText() {
-    //TODO add your code here
+    Text(text = stringResource(id = R.string.jetpack_compose),
+        fontStyle = FontStyle.Italic,
+        color = colorResource(id = R.color.colorPrimary),
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
